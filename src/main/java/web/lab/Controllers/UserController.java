ae1cfecf3d3a2a43b07ff06c62a1b8d47c013d58
Hottabych55 <ilik1799@gmail.com>
2020-08-10 15:08:04 +0300

    https://github.com/Hottabych55/lab/issues/6 Added method login, also added security

--- src/main/java/web/lab/Controllers/UserController.java
+++ src/main/java/web/lab/Controllers/UserController.java
@@ -1,24 +1,38 @@
 package web.lab.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import web.lab.Entities.User;
 import web.lab.Requests.RequestUser;
 import web.lab.Responses.ResponseUser;
+import web.lab.Security.TokenProvider;
 import web.lab.Services.UserService;
 import web.lab.Validation.UserValidation;
 
 @RestController
 public class UserController {
 
-    @Autowired
-    UserService userService;
 
     @Autowired
-    UserValidation userValidation;
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserValidation userValidation;
 
 
     //Токен при регистрации пуст
@@ -27,11 +41,25 @@ public class UserController {
         String nickname = request.getNickname();
         String password = request.getPassword();
         String validationResult = userValidation.validate(nickname, password);
-        if (validationResult.equals("OK")) {
-            userService.addUser(new User(nickname, password, null));
-            return new ResponseUser("Регистрация прошла успешна","NO");
-        }else{
+        if (!validationResult.equals("OK")) {
             return new ResponseUser(validationResult, "YES");
+        }else{
+            User user = new User(nickname, passwordEncoder.encode(password), null);
+            userService.addUser(user);
+            return new ResponseUser("Регистрация прошла успешно", "NO");
         }
+    }
+
+    @CrossOrigin
+    @PostMapping("/login")
+    public @ResponseBody ResponseUser login(@RequestBody RequestUser request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getNickname(), request.getPassword()));
+        User currentUser = userService.findUserByNickname(request.getNickname());
+
+        if (currentUser == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        String token = tokenProvider.resolveToken(request.getNickname());
+        return new ResponseUser(token, "NO");
     }
 }
